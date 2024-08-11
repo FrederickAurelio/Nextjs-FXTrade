cfff4befb7e61de9dd4166cdf4b8cb9c8d5d6632
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2024-08-11 13:01:30 +0800

    Fixes some bugs

--- app/auth/callback/github/route.ts
+++ app/auth/callback/github/route.ts
@@ -50,16 +50,6 @@ export async function GET(request: Request) {
         });
 
         if (error) throw new Error(error.message);
-      }
-
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}/app`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
         return NextResponse.redirect(`${origin}/app`);
       }
     }
--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -12,16 +12,7 @@ export async function GET(request: Request) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}/reset?link=true`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}/reset?link=true`);
-      }
+      return NextResponse.redirect(`${origin}/reset?link=true`);
     }
   }
 
